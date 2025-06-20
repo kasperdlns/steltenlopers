2fc0063bd97d53d93789116d0ee57a18d02ad8d3
Kasper Daelemans <kasperdaelemans@MacBook-Air-van-Kasper.local>
2025-06-20 09:54:36 +0200

    pagina titles

--- contact.php
+++ contact.php
@@ -61,7 +61,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>contact</title>
+    <title>Contact</title>
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">
     <script src="https://kit.fontawesome.com/d9196de2ad.js" crossorigin="anonymous"></script>
