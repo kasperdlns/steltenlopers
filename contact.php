03e7bc554aa0c43a39df5d963fd5168b6024f2ee
Kasper Daelemans <kasperdaelemans@MacBook-Air-van-Kasper.local>
2025-08-01 10:44:25 +0200

    nieuwe link icons contact

--- contact.php
+++ contact.php
@@ -64,7 +64,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Contact</title>
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">
-    <script src="https://kit.fontawesome.com/d9196de2ad.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="icon" href="images/logo.jpg" type="image/png">
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
